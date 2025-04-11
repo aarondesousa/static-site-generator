bb796132ee559923783c65ded995f3d13539fb68
aarondesousa <aarondesousa1@gmail.com>
2025-04-11 22:18:32 +0530

    add generator to convert markdown to html page

---
+++ src/gencontent.py
@@ -0,0 +1,33 @@
+import os
+import re
+
+from markdown_blocks import markdown_to_html_node
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f" * {from_path} {template_path} -> {dest_path}")
+
+    with open(from_path, "r", encoding="utf-8") as f:
+        markdown = f.read()
+
+    with open(template_path, "r", encoding="utf-8") as f:
+        template = f.read()
+
+    html = markdown_to_html_node(markdown).to_html()
+    title = extract_title(markdown)
+
+    output = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+
+    dest_dir = os.path.dirname(dest_path)
+    if dest_dir:
+        os.makedirs(dest_dir, exist_ok=True)
+
+    with open(dest_path, "w", encoding="utf-8") as f:
+        f.write(output)
+
+
+def extract_title(markdown):
+    match = re.search(r"^#[ \t]+(.*)", markdown, re.MULTILINE)
+    if not match:
+        raise ValueError("no title found")
+    return match.group(1)
--- src/main.py
+++ src/main.py
@@ -2,21 +2,28 @@ import os
 import shutil
 
 from copystatic import copy_files_recursive
-
-dir_path_static = "./static"
-dir_path_public = "./public"
+from gencontent import generate_page
 
 
 def main():
-    print(f'Deleting "{dir_path_public}"...')
-    if os.path.exists(dir_path_public):
-        if os.path.isdir(dir_path_public):
-            shutil.rmtree(dir_path_public)
-        elif os.path.isfile(dir_path_public):
-            os.remove(dir_path_public)
+    static_dir = os.path.join(".", "static")
+    public_dir = os.path.join(".", "public")
+    content_md = os.path.join(".", "content", "index.md")
+    template_path = os.path.join(".", "template.html")
+    output_path = os.path.join(public_dir, "index.html")
 
-    print(f'Copying files from "{dir_path_static}" to "{dir_path_public}"...')
-    copy_files_recursive(dir_path_static, dir_path_public)
+    print(f'Deleting "{public_dir}"...')
+    if os.path.exists(public_dir):
+        if os.path.isdir(public_dir):
+            shutil.rmtree(public_dir)
+        elif os.path.isfile(public_dir):
+            os.remove(public_dir)
+
+    print(f'Copying files from "{static_dir}" to "{public_dir}"...')
+    copy_files_recursive(static_dir, public_dir)
+
+    print("Generating page...")
+    generate_page(content_md, template_path, output_path)
 
 
 if __name__ == "__main__":
